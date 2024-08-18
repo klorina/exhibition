9780239715b24f4b667c14221dac16574c29dc60
klorina <175967051+klorina@users.noreply.github.com>
2024-08-18 22:01:57 +0900

    edit js_1.js

--- JavaScript/js_1.js
+++ JavaScript/js_1.js
@@ -7,7 +7,7 @@ function move() {
   x = circle.style.left;
   x = Number(x.slice(0, -2))
   x = (x + 5);
-  if (window.innerWidth <= (x + 80 * 2)) {
+  if (window.innerWidth <= (x + 80)) {
     x = 0;
   }
   circle.style.left = String(x) + "px"
