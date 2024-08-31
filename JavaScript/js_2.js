6c80e95a81bafd8c6c32a1921229080d4c8acf08
klorina <175967051+klorina@users.noreply.github.com>
2024-08-31 12:54:11 +0900

    Add:  style.css Update:  index.html,  JavaScript/js_2.html

--- JavaScript/js_2.js
+++ JavaScript/js_2.js
@@ -1,38 +1,61 @@
 const content = document.getElementById("content");
-const word_1_li = ["傘", "剣", "指", "針"];
 
-const getIP = new Promise((resolve) => {
+const process = async () => {
   const param = new URLSearchParams(document.location.search);
-  let ip = param.get("emu");
-  if ( ip==null || !(/(\d{1,3}\.){3}\d{1,3}/.test(ip))) {
-    fetch("https://ipinfo.io?callback")
-      .then((res) => res.json())
-      .then((json) => {resolve(json["ip"])})
+  const eip = param.get("emu");
+  const cond = eip == null || !(/(\d{1,3}\.){3}\d{1,3}/.test(eip)) //emuがnull||正しくないIPアドレスの時
+  if (cond) {
+    const res = await fetch("https://ipinfo.io?callback")
+    const json = await res.json()
+    editpage(json.ip)
   } else {
-    resolve(ip);
+    editpage(eip);
   }
-})
-
-getIP
-  .then((ip) => {
-    makePage(ip);
-    setLink(ip);  })
-
-function makePage(ip) {
-  const ip_num = ip.split('.');
-  const word_1 = word_1_li[Math.floor(ip_num[0]/64)];
-
-  addLine(`空に${word_1}を差した｡`);
 }
 
-function addLine(textContent) {
-  const newParadigm = document.createElement("p");
-  const newContent = document.createTextNode(textContent);
-  newParadigm.appendChild(newContent);
-  content.insertAdjacentElement("BeforeEnd", newParadigm);
+const editpage = (ip) => {
+  makePage(ip);
+  setLink(ip);
 }
 
-function setLink(ip) {
+const makePage = async (ip) => {
+  const file = await fetch("js_2.json")
+  const article = temp[choice(ip, 0)][choice(ip, 1)]
+  let words_map = new Map();
+  Object.keys(article.Words).forEach((key, idx) => {
+    words_map.set(key, article.Words[key][choice(ip, 15 - idx)])
+  })
+  words_map.set("?_IP", ip)
+  const words = Object.fromEntries(words_map)
+  let cont = article.Content;
+  Object.keys(words).forEach((key) => {
+    cont = cont.map((prd) => prd.replace(key, words[key]))
+  })
+  const a_cont = cont
+  addTitle(article.Title)
+  a_cont.forEach((paradigm) => {
+    addLine(paradigm)
+  });
+}
+
+const choice = (ip, idx) => {
+  const ip_num = ip.split('.')[Math.floor(idx/4)]
+  return Math.floor(ip_num/Math.pow(4, 3 - idx % 4)) % 4
+}
+
+const addTitle = (title) => {
+  const newTitle = document.createElement("h1");
+  const newContent = document.createTextNode(title);
+  newTitle.appendChild(newContent);
+  content.insertAdjacentElement("BeforeEnd", newTitle);
+}
+
+const addLine = (htmlContent) => {
+  const newParadigm = "<p>" + htmlContent + "</p>";
+  content.insertAdjacentHTML("BeforeEnd", newParadigm);
+}
+
+const setLink = (ip) => {
   const ip_num = ip.split('.');
   const ip_1 = document.getElementById("IP_1");
   const ip_2 = document.getElementById("IP_2");
@@ -43,6 +66,8 @@ function setLink(ip) {
   ip_3.value=ip_num[2];
   ip_4.value=ip_num[3];
 }
+
+process();
 
 function go() {
   const ip_1 = document.getElementById("IP_1");
