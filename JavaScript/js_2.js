29735d5439815caccd6fc723f3242f218473d566
klorina <175967051+klorina@users.noreply.github.com>
2024-09-01 16:26:18 +0900

    modified:   JavaScript/js_1.html new file:   JavaScript/js_2.css modified:   JavaScript/js_2.html modified:   JavaScript/js_2.js modified:   JavaScript/js_2.json deleted:    JavaScript_CSS/js_css_1.html modified:   index.html

--- JavaScript/js_2.js
+++ JavaScript/js_2.js
@@ -1,6 +1,7 @@
 const content = document.getElementById("content");
+let now = content;
 
-const process = async () => {
+async function process() {
   const param = new URLSearchParams(document.location.search);
   const eip = param.get("emu");
   const cond = eip == null || !(/(\d{1,3}\.){3}\d{1,3}/.test(eip)); //emuがnull||正しくないIPアドレスの時
@@ -21,16 +22,20 @@ const editpage = (ip) => {
 const makePage = async (ip) => {
   const file = await fetch("./js_2.json");
   const json_dat = await file.json();
-  const article = json_dat[choice(ip, 0)][choice(ip, 1)];
+  const article = json_dat[choice(ip, 2)][choice(ip, 3)];
   let words_map = new Map();
-  Object.keys(article.Words).forEach((key, idx) => {
-    words_map.set(key, article.Words[key][choice(ip, 15 - idx)]);
+  Object.keys(article.Words).forEach((key) => {
+    words_map.set(key, (article.Words[key].word)[choice(ip, article.Words[key].using)]);
   })
   words_map.set("?_IP", ip);
   const words = Object.fromEntries(words_map);
   let cont = article.Content;
   Object.keys(words).forEach((key) => {
-    cont = cont.map((prd) => prd.replace(key, words[key]));
+    cont.forEach((prd, idx) => {
+      if (typeof(prd) == 'string') {
+        cont[idx] = prd.replaceAll(key, words[key])
+      }
+    })
   })
   const a_cont = cont;
   addTitle(article.Title);
@@ -51,9 +56,22 @@ const addTitle = (title) => {
   content.insertAdjacentElement("BeforeEnd", newTitle);
 }
 
-const addLine = (htmlContent) => {
-  const newParadigm = "<p>" + htmlContent + "</p>";
-  content.insertAdjacentHTML("BeforeEnd", newParadigm);
+const addLine = (Content) => {
+  if (typeof(Content) == "string") {
+    const newParadigm = "<p>" + Content + "</p>";
+    now.insertAdjacentHTML("BeforeEnd", newParadigm);
+  } else {
+    if (Content.type == "begin") {
+      const newTag = document.createElement(Content.tag)
+      Object.keys(Content.attribute).forEach((attr) => {
+        newTag.setAttribute(attr, Content.attribute[attr])
+      })
+      now.insertAdjacentElement("BeforeEnd", newTag)
+      now = newTag
+    } else {
+      now = now.parentElement
+    }
+  }
 }
 
 const setLink = (ip) => {
@@ -77,4 +95,13 @@ function go() {
   const ip_4 = document.getElementById("IP_4");
 
   location.href = `js_2.html?emu=${ip_1.value}.${ip_2.value}.${ip_3.value}.${ip_4.value}`;
+}
+
+function rand() {
+  const ip_1 = Math.floor(Math.random() * 256);
+  const ip_2 = Math.floor(Math.random() * 256);
+  const ip_3 = Math.floor(Math.random() * 256);
+  const ip_4 = Math.floor(Math.random() * 256);
+
+  location.href = `js_2.html?emu=${ip_1      }.${ip_2      }.${ip_3      }.${ip_4      }`;
 }
